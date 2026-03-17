15d8074c49a97c42a4e8624d1eddf7e7e8f81447
Mykola Radionov <apelsinbanan07@gmail.com>
2026-03-17 20:30:51 +0200

    added terrafrom v2

--- main.tf
+++ main.tf
@@ -33,3 +33,22 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_instance" "web_server" {
+    ami      = data.aws_ami.ubuntu.id
+    instance_type    = "t2.micro"
+
+    vps_security_group_ids   = [aws_security_group.web_sg.id]
+    user_data = <<- EQF
+                 #!/bin/bash
+                 sudo apt-get update
+                 sudo apt-get install -y docker.io
+                 sudo systemctl start docker
+                 sudo systemctl enable docker 
+                 sudo docker run -d -p 80:80 24024102/ci-cd-v2:latest
+                 EQF
+
+}
+tags = {
+    
+}
