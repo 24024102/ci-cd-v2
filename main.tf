923c12a97dd8b52bf066a213a44e71a9708dae54
Mykola Radionov <apelsinbanan07@gmail.com>
2026-03-17 20:21:20 +0200

    added terrafrom v1

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "us-east-1" 
+}
+data "aws_ami" "ubuntu" {
+    most_recent = true
+    filter {
+        name = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+    owners = ["099720109477"]
+}
+
+resource "aws_security_group" "web_sg" {
+    name      = "fastapi_web_sg"
+    description     = "Allow HTTP web trafic"
+    ingress {
+    description = "Allow HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Пускаем всех
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
